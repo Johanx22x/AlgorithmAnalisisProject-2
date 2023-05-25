4e4ee42c3406e691da0ec01b70b302d46739684f
Johanx22x <johanrodsa2210@gmail.com>
2023-05-25 04:01:51 +0000

    Add ErrorCodes enum

---
+++ src/main/java/com/algorithmanalysis/secondproject/utils/ErrorCodes.java
@@ -0,0 +1,15 @@
+package com.algorithmanalysis.secondproject.utils;
+
+/**
+ * Error codes 
+ *
+ * This class is responsible for managing the error codes 
+ *
+ * @autor Johan Rodriguez
+ * @version 1.0
+ */
+public enum ErrorCodes {
+    ERROR_INCAPABLE, // The program can't generate the desired result
+    MAX_ATTEMPTS_EXCEEDED, // The program can't generate the desired result
+    NO_ERROR // The program can generate the desired result
+}
